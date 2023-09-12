0baea9b694a1efbc81727d5a66f2e3440f291c75
Yuri Avdijevski <dtoxvanilla@gmail.com>
2023-09-12 23:43:23 +0100

    chore: just adding some TS clarity

--- src/device/geolocation/AppGeolocation.tsx
+++ src/device/geolocation/AppGeolocation.tsx
@@ -1,15 +1,15 @@
 import { GeolocationType } from "../../types";
 import IGeolocation from "./IGeolocation";
-import { Geolocation } from "@capacitor/geolocation";
+import { Geolocation, PermissionStatus } from "@capacitor/geolocation";
 
 export default class AppGeolocation implements IGeolocation {
   constructor() {}
 
   async getCurrentLocationAsync(): Promise<GeolocationType | undefined> {
-    let hasPermission = false;
+    let hasPermission: boolean = false;
 
     try {
-      const status = await Geolocation.checkPermissions();
+      const status: PermissionStatus = await Geolocation.checkPermissions();
       if (
         status.location === "granted" ||
         status.coarseLocation === "granted"
@@ -20,7 +20,7 @@ export default class AppGeolocation implements IGeolocation {
 
     if (!hasPermission) {
       try {
-        const status = await Geolocation.requestPermissions();
+        const status: PermissionStatus = await Geolocation.requestPermissions();
 
         if (
           status.location === "granted" ||
